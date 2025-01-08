cfb6599b93a2311aef884dd8b518ff2df195ec02
grahacr <courtney.graham@atlasschool.com>
2025-01-08 16:27:03 -0600

    correctly render app by connecting it to root

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
 import "./index.css";
+import reactDOM from "react-dom/client";
+import App from "./App";
 
 // Render the App component to the root element
+
+const div = document.getElementById("root");
+const root = reactDOM.createRoot(div);
+root.render(<App />);
