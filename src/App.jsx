5fe615ede5ef3d15125638f88b4aed22c4827219
grahacr <courtney.graham@atlasschool.com>
2025-01-13 13:30:18 -0600

    oops forgot to commit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import Header from "./assets/components/Header";
+import Section from "./assets/components/Section";
 
 export default function App() {
   return <div className="app">
     <Header />
+    <Section title="What is react?">
+      <p>React is a component-based library used with Javascript for the purpose of building User Interfaces. It follows the virtual DOM approach which facilitates effecient rendering performance</p>
+    </Section>
+    <Section title="Benefits of react">
+      <ul>
+        <li>Organized</li>
+        <li>Scalable</li>
+        <li>Reusable components</li>
+        <li>Simple</li>
+        <li>Fast, effecient rendering</li>
+      </ul>
+    </Section>
   </div>;
 }
